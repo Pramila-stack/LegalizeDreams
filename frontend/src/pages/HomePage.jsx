2ad02d184f0b22572374bfc99272666337b40103
Sugam Gurung <sugamgurung2004@gmail.com>
2026-07-17 19:29:57 +0545

    Revert "feat: add video carousel with animated transitions to homepage"
    This reverts commit 3aabfcd1a4ea2b440fa9bd88967dffec67e11acb.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import Hero from '../components/home/Hero'
-import VideoCarousel from '../components/home/VideoCarousel'
 import CategoryGrid from '../components/home/CategoryGrid'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import FaqAccordion from '../components/home/FaqAccordion'
@@ -8,7 +7,6 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <VideoCarousel />
       <CategoryGrid />
       <FeaturedProducts />
       <FaqAccordion />
